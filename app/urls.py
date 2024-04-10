747b140cb3370f82ee8c32dd5cc3eef07df92744
Pujan Maharjan <pujanmaharjan22@gmail.com>
2024-04-10 23:12:19 +0545

    handeling not found and some previous problem

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,6 @@ from app.views import *
 #from app import views (we can also write it as this and replave app with .)
 
 urlpatterns = [
-    path('', home), #but we have to put views.hello instead of hello
-    path('job/<int:id>', job_detail),
+    path('', home, name="job_home"), #but we have to put views.hello instead of hello
+    path('job/<int:id>', job_detail, name="job_detail"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 import http
+from os import execv
 from django.shortcuts import redirect, render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
+from django.urls import reverse
 # Create your views here.
 job_title = [
     '',
@@ -15,13 +17,16 @@ def home(request):
     job_list = "<ul>"
     for i in job_title:
         job_id = job_title.index(i)
-        job_list += f"<li><a href='job/{job_id}'>{i}</a></li>"
+        detail_url = reverse('job_detail', args=(job_id, ))
+        job_list += f"<li><a href='{detail_url}'>{i}</a></li>"
     job_list += "</ul>"
     return HttpResponse(f"<h1>Hello World!!!</h1> {job_list}")
 
 def job_detail(request, id):
-    if id == 0:
-        return redirect("/")
-
-    site = f"<h1>{job_title[id]}</h1> {job_desc[id]}"
-    return HttpResponse(site)
+    try:
+        if id == 0:
+            return redirect(reverse("job_home"))
+        site = f"<h1>{job_title[id]}</h1> {job_desc[id]}"
+        return HttpResponse(site)
+    except:
+        return HttpResponseNotFound("Not Found")
