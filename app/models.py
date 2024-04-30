893b6715bab685ecd677a9922042ad8044fdb8f5
Pujan Maharjan <pujanmaharjan22@gmail.com>
2024-04-30 23:09:45 +0545

    one-to-one relation

--- app/models.py
+++ app/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from django.utils.text import slugify
 
 # Create your models here.
+    
+class Location(models.Model):
+    street = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+    zip = models.CharField(max_length=200)
+
+
 class JobPost(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
@@ -9,6 +18,7 @@ class JobPost(models.Model):
     expiry = models.DateField(null=True)
     salary = models.IntegerField()
     slug = models.SlugField(null=True, max_length=50, unique=True)
+    location = models.OneToOneField(Location, on_delete=models.CASCADE, null=True)
 
     def save(self, *args, **kwargs):
         if not self.id:
