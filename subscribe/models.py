4f964edbd5f68fbd8728e6b35d27253eed76d504
Pujan Maharjan <pujanmaharjan22@gmail.com>
2024-05-01 23:51:36 +0545

    validation and connection to database in form

--- subscribe/models.py
+++ subscribe/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Subscribe(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
--- subscribe/views.py
+++ subscribe/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from subscribe.models import Subscribe
 
 # Create your views here.
 def subscribe(request):
-    context={}
+    err_email = ""
+    if request.POST:
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        email = request.POST['email']
+        print("Post request: ", email)
+        if email == "":
+            err_email = "Please enter email"
+
+        subscribe = Subscribe(first_name=first_name, last_name=last_name, email=email)
+        subscribe.save()
+
+    context={"err_email" : err_email}
     return render(request, 'subscribe/subscribe.html', context)
