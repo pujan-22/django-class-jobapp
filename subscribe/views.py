7d23470799a75b7dde06c32072e6c13086a713ad
Pujan Maharjan <pujanmaharjan22@gmail.com>
2024-05-05 22:22:46 +0545

    form with modelform and save

--- subscribe/views.py
+++ subscribe/views.py
@@ -10,13 +10,14 @@ def subscribe(request):
     err_email = ""
     if request.POST:
         if subscribe_form.is_valid():
-            print("valid form")
-            email= subscribe_form.cleaned_data['email']
-            first_name= subscribe_form.cleaned_data['first_name']
-            last_name= subscribe_form.cleaned_data['last_name']
-            print(email, first_name, last_name)
-            subscribe = Subscribe(first_name=first_name, last_name=last_name, email=email)
-            subscribe.save()
+            subscribe_form.save()
+            # print("valid form")
+            # email= subscribe_form.cleaned_data['email']
+            # first_name= subscribe_form.cleaned_data['first_name']
+            # last_name= subscribe_form.cleaned_data['last_name']
+            # print(email, first_name, last_name)
+            # subscribe = Subscribe(first_name=first_name, last_name=last_name, email=email)
+            # subscribe.save()
             return redirect(reverse('thank_you'))
     context={"form":subscribe_form ,"err_email":err_email}
     return render(request, 'subscribe/subscribe.html', context)
