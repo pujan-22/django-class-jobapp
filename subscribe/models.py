111c87ce80e11cc5ab14cf9625f007180b218a94
Pujan Maharjan <pujanmaharjan22@gmail.com>
2024-05-05 23:12:33 +0545

    validators

--- subscribe/models.py
+++ subscribe/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Subscribe(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
