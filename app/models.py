8606a755b0a9a53470d785f2f764a999739aeb3c
Pujan Maharjan <pujanmaharjan22@gmail.com>
2024-05-09 01:00:53 +0545

    updated models as per requirement

--- app/models.py
+++ app/models.py
@@ -20,8 +20,12 @@ class Location(models.Model):
 
 
 class JobPost(models.Model):
+    JOB_TYPE = [
+        ('Full Time', 'Full Time'),
+        ('Part Time', 'Part Time')
+    ] 
     title = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
+    description = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     expiry = models.DateField(null=True)
     salary = models.IntegerField()
@@ -29,6 +33,7 @@ class JobPost(models.Model):
     location = models.OneToOneField(Location, on_delete=models.CASCADE, null=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
     skills = models.ManyToManyField(Skills)
+    type = models.CharField(max_length=100, null=False, choices=JOB_TYPE)
 
     def save(self, *args, **kwargs):
         if not self.id:
